41d16bf4728d8930f02134035a7739fcce3c5760
salin <mursalin9040@gmail.com>
2023-10-10 09:47:51 +0800

    tipe data

---
+++ Day2.java
@@ -0,0 +1,20 @@
+package ngoding;
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = "Mursalin";
+        String nim = "D0221545";
+        int jumlah_sdr = 100 ;
+        char ukuran_bj = 'L';
+        float tinggi_bdn = 175;
+        double berat_bdn = 50.5;
+        boolean status_mhs = true;
+        System.out.println("Nama            : "+nama);
+        System.out.println("Nim             : "+nim);
+        System.out.println("Ukuran Baju     : "+ukuran_bj);
+        System.out.println("Berat Badan     : "+berat_bdn);
+        System.out.println("Tinggi Bandan   : "+tinggi_bdn);
+        System.out.println("Jumlah Saudara  : "+jumlah_sdr);
+        System.out.println("Satus Mahasiswa : "+status_mhs);
+    }
+    
+}
