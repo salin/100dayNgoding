73230e5de836d51934cf7d3f5d8a3f6281c58ff3
salin <mursalin9040@gmail.com>
2023-10-11 21:43:47 +0800

    tipe data

---
+++ Day3.java
@@ -0,0 +1,12 @@
+package ngoding;
+public class Day3 {
+    public static void main(String[] args) {
+        long nilaiLong = 123456789L;
+        short nilaiShort = 32767;
+        byte nilaiByte = 127;
+        
+        System.out.println("Long    : "+nilaiLong);
+        System.out.println("Short   : "+nilaiShort);
+        System.out.println("Byte    : "+nilaiByte);
+    }
+}
