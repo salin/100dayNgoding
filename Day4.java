ccf927206d9e074e9199049a5ca4ec987a7a062b
salin <mursalin9040@gmail.com>
2023-10-12 22:23:42 +0800

    boolean

---
+++ Day4.java
@@ -0,0 +1,11 @@
+package ngoding;
+public class Day4 {
+    public static void main(String[] args) {
+         int angka1 = 20;
+         int angka2 = 11;
+         
+         boolean hasil = angka1 != angka2;
+         System.out.println("apakah angka 1 tidak sama dengan angka 2"+ hasil);
+    }
+    
+}
