005694c98c404f57e01105a6459c7c4e2af48ee5
salin <mursalin9040@gmail.com>
2023-10-13 21:14:15 +0800

    operasi perhitungan

---
+++ Day5.java
@@ -0,0 +1,18 @@
+package ngoding;
+public class Day5 {
+    public static void main(String[] args) {
+        int angkaA = 100;
+        int angkaB = 50;
+        
+        int hasil = angkaA * angkaB;
+        int hasil1 = angkaA + angkaB;
+        int hasil2 = angkaA / angkaB;
+        int hasil3 = angkaA - angkaB;
+        
+        System.out.println("Hasil Perkalian : "+hasil);
+        System.out.println("Hasil Penjumlahan : "+hasil1);
+        System.out.println("Hasil Pembagian : "+hasil2);
+        System.out.println("Hasil Pengurangan : "+hasil3);
+    }
+    
+}
