351576fc9d86b64226123281921b27555116fa7c
salin <mursalin9040@gmail.com>
2023-10-14 20:53:26 +0800

    operator

---
+++ Day6.java
@@ -0,0 +1,12 @@
+package ngoding;
+public class Day6 {
+    public static void main(String[] args) {
+        int angka1 = 100;
+        int angka2 = 50;
+
+        // Operator "sama dengan" (==)
+        boolean hasilSamaDengan = angka1 == angka2;
+        System.out.println("Apakah angka1 sama dengan angka2? " + hasilSamaDengan);
+    
+    }    
+}
