b94e605aee162ef2b2bded7c0c6fbce549ddb176
salin <mursalin9040@gmail.com>
2023-10-15 20:32:55 +0800

    perbandingan

---
+++ Day7.java
@@ -0,0 +1,13 @@
+package ngoding;
+public class Day7 {
+    public static void main(String[] args) {
+       int a, b;
+        a = 10;
+        b = 5;
+        
+        boolean hasil = a > b ;
+        System.out.print("Apakah nilai a lebih atau sama dengan nilai b? : "+ hasil);
+        
+    }
+    
+}
