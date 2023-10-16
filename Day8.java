d1acfd282df9b95c4789587305095b5905de32c3
salin <mursalin9040@gmail.com>
2023-10-16 21:26:06 +0800

    tipe data katakter

---
+++ Day8.java
@@ -0,0 +1,12 @@
+package ngoding;
+
+public class Day8 {
+
+    public static void main(String[] args) {
+        char grade = 'A';
+        char nextGrade = (char) (grade + 1);
+        System.out.println("Grade selanjutnya (char):" + nextGrade);
+
+    }
+
+}
