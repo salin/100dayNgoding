052f9954d0fcf6b08ebd5422aa1ed4931136faf5
salin <mursalin9040@gmail.com>
2023-10-17 21:01:10 +0800

    menghitung volume kubus

---
+++ Day9.java
@@ -0,0 +1,13 @@
+package ngoding;
+
+public class Day9 {
+    public static void main(String[] args) {
+        String nama = "balok";
+        int panjang = 8;
+        int lebar = 6;
+        int tinggi = 4;
+        int hasil   = panjang*lebar*tinggi;
+        
+        System.out.println("volume = " + hasil);
+    }    
+}
