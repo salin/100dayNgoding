04f17bd32efd5049864ea696a3f45be304498dfd
salin <mursalin9040@gmail.com>
2023-10-18 20:21:40 +0800

    menghitung volume balok

---
+++ Day10.java
@@ -0,0 +1,13 @@
+package ngoding;
+public class Day10 {
+    public static void main(String[] args) {
+        // Menentukan panjang sisi kubus
+        int sisi = 40; // Ganti dengan panjang sisi yang Anda inginkan
+
+        // Menghitung volume kubus
+        int volume = sisi * sisi * sisi;
+
+        // Menampilkan hasil
+        System.out.println("Volume kubus adalah: " + volume);
+    }    
+}
