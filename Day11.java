58d56871dde3b859f3b892204f5b74bdcf20ea12
salin <mursalin9040@gmail.com>
2023-10-19 23:45:38 +0800

    Array

---
+++ Day11.java
@@ -0,0 +1,9 @@
+public class ArraySatuDimensi {
+    public static void main(String[] args) {
+        // Mendeklarasikan dan menginisialisasi array satu dimensi
+        int[] angka = {1, 2, 3, 4, 5};
+
+        // Mengakses elemen array
+        System.out.println("Elemen kedua: " + angka[1]);   
+    }
+}
