659e1affa98a57bf313b76013cb4f0761620d059
salin <mursalin9040@gmail.com>
2023-10-20 21:00:38 +0800

    Array2D

---
+++ Day12.java
@@ -0,0 +1,20 @@
+package ngoding;
+public class Day12 {
+    public static void main(String[] args) {
+        int[][] array2D = new int[2][2];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                array2D[i][j] = i * 2 + j + 1;
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(array2D[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+    
+   
