fc0b554f2d28809f63851ab2469c97a4a68a24da
salin <mursalin9040@gmail.com>
2023-10-21 20:27:11 +0800

    Kondisi

---
+++ Day13.java
@@ -0,0 +1,12 @@
+package ngoding;
+public class Day13 {
+    public static void main(String[] args) {
+        int number = 7;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " adalah angka genap.");
+        } else {
+            System.out.println(number + " adalah angka ganjil.");
+        }
+    }
+}
