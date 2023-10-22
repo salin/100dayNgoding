1265b9817b28f39d1e7f250b23339deaff57e155
salin <mursalin9040@gmail.com>
2023-10-22 22:18:05 +0800

    array 1D

---
+++ Day14.java
@@ -0,0 +1,12 @@
+package ngoding;
+
+public class Day14 {
+ 
+    public static void main(String[] args) {
+        int[] angka = {5, 10, 15, 20, 25};
+        System.out.println("Isi array angka:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Angka ke-" + i + ": " + angka[i]);
+        }
+    }
+}
