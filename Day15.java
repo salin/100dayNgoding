a0ed28bc7adcca4557102d2e9e70d84daa4f8297
salin <mursalin9040@gmail.com>
2023-10-23 20:41:22 +0800

    for

---
+++ Day15.java
@@ -0,0 +1,9 @@
+package ngoding;
+public class Day15 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("angka = "+i);
+        }
+    }
+    
+}
