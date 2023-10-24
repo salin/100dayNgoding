33b92cb6e4dc4891bd3a83678318aed1d34540dc
salin <mursalin9040@gmail.com>
2023-10-24 21:33:38 +0800

    sisa bagi

---
+++ Day16.java
@@ -0,0 +1,10 @@
+package ngoding;
+public class Day16 {
+    public static void main(String[] args) {
+        int A = 11;
+        int B = 7;
+        int hasil = A % B;
+        System.out.println("Hasil Baginya   : "+hasil);
+    }
+    
+}
