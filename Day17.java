26ae4164676c17a90ff5c6516773012a352ab438
salin <mursalin9040@gmail.com>
2023-10-25 20:12:10 +0800

    Do While

---
+++ Day17.java
@@ -0,0 +1,13 @@
+package ngoding;
+
+public class Day17 {
+    public static void main(String[] args) {
+        int count = 1;
+
+        do {
+            System.out.println("Perulangan ke-" + count);
+            count++; // Menambah nilai count
+        } while (count <= 5);
+    }
+}
+
