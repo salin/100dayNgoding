0a8ba6022c917fb550effb096c444f7eb6315cbd
salin <mursalin9040@gmail.com>
2023-10-27 20:51:38 +0800

    inputan

---
+++ Day19.java
@@ -0,0 +1,14 @@
+package ngoding;
+
+import java.util.Scanner;
+
+public class Day19 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("masukkan gaji : ");
+        int gaji = input.nextInt();
+        System.out.println(gaji);
+    }
+}
