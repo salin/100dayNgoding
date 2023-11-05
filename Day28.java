e55b1badb805a299cd754faee5ae1b6707b61d47
salin <mursalin9040@gmail.com>
2023-11-05 20:58:21 +0800

    decrement

---
+++ Day28.java
@@ -0,0 +1,13 @@
+package ngoding;
+import java.util.Scanner;
+public class Day28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("masukkan nilai  : ");
+        int nilai = sc.nextInt();
+        for (int i = nilai ; i >= 0; i--) {
+            System.out.println(i);
+            
+        }
+    }
+}
