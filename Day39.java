18b0c1a93c26b227ced7831993fb364442d19847
salin <mursalin9040@gmail.com>
2023-11-16 21:21:44 +0800

    looping pada array

---
+++ Day39.java
@@ -0,0 +1,13 @@
+package ngoding;
+
+public class Day39 {
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6};
+        int jml = a.length;
+        for (int i = 0 ; i < jml; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println(""); 
+    }
+    
+}
