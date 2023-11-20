051779a5932ed6e6161d8070c84370d56afb5b69
salin <mursalin9040@gmail.com>
2023-11-20 20:48:05 +0800

    pola segiempat

---
+++ Day43.java
@@ -0,0 +1,16 @@
+package ngoding;
+
+public class Day43 {
+    public static void main(String[] args) {
+        int angka = 3 ;
+        for (int i = 1; i <= angka ; i++) {
+            for (int j = 1; j < angka; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("* ");
+            
+        }
+        System.out.println("");
+    }
+    
+}
