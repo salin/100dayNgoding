f74a9cfb7f6b634e9a0f1c2666426b293363b242
salin <mursalin9040@gmail.com>
2023-11-24 20:52:50 +0800

    menampilkan angka ganjil menggunakan while

---
+++ Day47.java
@@ -0,0 +1,15 @@
+package ngoding;
+
+public class Day47 {
+    public static void main(String[] args) {
+        int b = 1;
+        while(b <= 10){
+            if(b % 2 == 1){
+                System.out.print(b+" ");
+            } 
+            b++;
+        }
+        System.out.println(" ");
+    }
+    
+}
