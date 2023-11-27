5515643778037acccf137270b357654844d57e83
salin <mursalin9040@gmail.com>
2023-11-27 19:53:10 +0800

    continue

---
+++ Day50.java
@@ -0,0 +1,15 @@
+package ngoding;
+
+public class Day50 {
+
+    public static void main(String[] args) {
+        for (int A = 0; A < 8; A++) {
+            if (A == 5) {
+                continue;
+            } else {
+                System.out.print(A);
+            }
+            System.out.println("");
+        }
+    }
+}
