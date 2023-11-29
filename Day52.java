58d2307e3f29b8c9ef496a14f5cfefe2cd5f10c2
salin <mursalin9040@gmail.com>
2023-11-29 21:20:14 +0800

    menentukan kelulusan menggunakan array

---
+++ Day52.java
@@ -0,0 +1,18 @@
+package ngoding;
+
+public class Day52 {
+
+    public static void main(String[] args) {
+        int[] array = {45, 78, 71, 89, 93, 58, 62, 63, 34, 78};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 70) {
+                System.out.println("lulus "+array[i]);
+            } else {
+                System.out.println("tidak lulus "+array[i]);
+            }
+
+        }
+
+    }
+
+}
