157f036600e9ab263bc77f61bffb28b6db502387
salin <mursalin9040@gmail.com>
2023-12-07 22:42:39 +0800

    menghitung celcius ke celvin

---
+++ Day60.java
@@ -0,0 +1,18 @@
+package ngoding;
+
+import java.util.Scanner;
+
+public class Day60 {
+    public static void main(String[] args) {
+        
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print (
+    "Masukkan suhu dalam Celsius: ");
+        double suhuCelsius = scanner.nextDouble();
+    double suhuKelvin = suhuCelsius + 273.15;
+    System.out.println ("Suhu dalam Kelvin: " + suhuKelvin);
+    
+}
+}
+
