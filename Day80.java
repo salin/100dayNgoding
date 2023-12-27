d12cf206bc7c9d62298b243dd2bddc0cc39455d5
salin <mursalin9040@gmail.com>
2023-12-27 20:34:27 +0800

    menentukan angka positif dan negatif

---
+++ Day80.java
@@ -0,0 +1,17 @@
+package ngoding;
+
+public class Day80 {
+
+    public static void main(String[] args) {
+
+        double angka = -12.3;
+
+        if (angka < 0.0) {
+            System.out.println(angka + "angka negatif");
+        } else if (angka > 0.0) {
+            System.out.println(angka + "angka positif");
+        } else {
+            System.out.println(angka + "angka 0  ");
+        }
+    }
+}
