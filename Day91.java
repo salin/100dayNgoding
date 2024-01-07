b5ec2248d5594daebdb3e526124077775b18806c
salin <mursalin9040@gmail.com>
2024-01-07 21:48:02 +0800

    mengurutkan abjad menggunakan foreach

---
+++ Day91.java
@@ -0,0 +1,23 @@
+package ngoding;
+
+import java.util.Arrays;
+
+public class Day91 {
+
+    public static void main(String[] args) {
+        String[] namaBuah = {
+            "Banana", "Apple", "Cherry", "Date", "Kiwi",
+            "Fig", "Lemon", "Honeydew", "Elderberry", "Grapes",
+            "Watermelon", "Nectarine", "Strawberry", "Papaya", "Quince",
+            "Raspberry", "Orange", "Tangerine", "Uva", "Mango"
+        };
+
+        Arrays.sort(namaBuah);
+
+        System.out.println("Dalam urutan abjad:");
+
+        for (String buah : namaBuah) {
+            System.out.println(buah);
+         }
+    }
+}
