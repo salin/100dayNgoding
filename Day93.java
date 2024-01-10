a752c93cd054a0a0621a93b1a9c8c7c2756dd2bc
salin <mursalin9040@gmail.com>
2024-01-10 20:38:58 +0800

    menghitung kelahiran

---
+++ Day93.java
@@ -0,0 +1,25 @@
+package ngoding;
+
+import java.util.Scanner;
+
+public class Day93 {
+    public static void main(String[] args) {
+        int jumlahOrang = 3; 
+        int[] tahunLahir = new int[jumlahOrang];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < tahunLahir.length; i++) {
+            System.out.print("Masukkan tahun lahir orang ke-" + (i + 1) + ": ");
+            tahunLahir[i] = scanner.nextInt();
+        }
+
+        int tahunSekarang = 2023;
+        System.out.println("\nUmur setiap orang:");
+        for (int i = 0; i < tahunLahir.length; i++) {
+            int umur = tahunSekarang - tahunLahir[i];
+            System.out.println("Orang ke-" + (i + 1) + ": " + umur + " tahun");
+        }
+    }
+}
+
+    
